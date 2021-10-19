5268a070976e2e72e63252f46614e0787c80dd20
ervalsa <ervalsananda@gmail.com>
2021-10-19 13:56:27 +0800

    update stack

---
+++ Stack/balance_bracket.py
@@ -0,0 +1,32 @@
+def BracketBalanced(expr):
+    stack = []
+
+    for char in expr:
+        if char in ["(", "{", "["]:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            current_char = stack.pop()
+            if current_char == '(':
+                if char != ")":
+                    return False
+            if current_char == "{":
+                if char != "}":
+                    return False
+            if current_char == "[":
+                if char != "]":
+                    return False
+
+    if stack:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    expr = "{()}[]"
+
+    if BracketBalanced(expr):
+        print("Balanced")
+    else:
+        print("Not Balanced")
